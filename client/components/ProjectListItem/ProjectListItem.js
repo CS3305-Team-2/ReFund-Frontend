d744762613a3438b018abab3188820a7c25bc220
Jack English <jackjenglish@gmail.com>
2019-03-04 00:05:26 +0000

    project list item added details

--- client/components/ProjectListItem/ProjectListItem.js
+++ client/components/ProjectListItem/ProjectListItem.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import styles from './ProjectListItem.scss';
 
+const friendlyStatus = status => {
+  const map = {
+    "DRAFT": 'Draft',
+    'SFI_APPROVED': 'Approved',
+    'RO_SUBMITTED': 'Awaiting RO Approval',
+    'RO_APPROVED': 'Awaiting SFI Approval',
+    'REJECTED': 'Rejected',
+  }
+  return map[status];
+}
+
 const ProjectListItem = (props) => {
   const project = props.project;
+  const proposal = project.proposal;
+
+  const proposalStatus = (
+    <div className={styles.detail}>
+      <span className={styles.label}>Proposal Status - </span>
+      {proposal ? friendlyStatus(proposal.status) : 'No Proposal'} 
+    </div>
+  )
   
   return (
     <div className={styles.root}>
       <div className={styles.container}>
         <div className={styles.title}>{project.name}</div>
+        {proposalStatus}
       </div>
     </div>
   );	
